c616b032c865120400b469939c72e420e8307a74
David T. <davidt.info@gmail.com>
2025-03-18 17:37:27 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bem-vindo ao mundo do useReducer!
         </p>
         <a
           className="App-link"
